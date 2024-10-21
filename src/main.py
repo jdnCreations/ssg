b50c46257824c49c81e7b2693b1b84d52f579c72
Jordan Needham <jdneedham@hotmail.com>
2024-10-21 20:23:29 +0800

    passed CH 5:1

--- src/main.py
+++ src/main.py
@@ -1,20 +1,48 @@
+from genericpath import isfile
 import os
+import shutil
 from textnode import TextNode
 
 
-def source_to_static():
+def source_to_destination():
+
     # delete all contents of destination folder
     if os.path.exists("public"):
         print("PUBLIC EXISTS!")
         files = os.listdir("public")
         for file in files:
-            os.remove("public/" + file)
+            file_path = os.path.join("public", file)
+            if os.path.isfile(file_path):
+                print(f"removing file {file_path}")
+                os.remove(file_path)
+            else:
+                print(f"removing folder {file_path}")
+                shutil.rmtree(file_path)
+    # create public directory if it doesnt exist
     else:
         os.mkdir("public")
 
+    # copy all contents inside source directory to destination directory
+    if os.path.exists("static"):
+        copyfiles("static", "public")
+
+
+def copyfiles(from_location, to_location):
+    files = os.listdir(from_location)
+    for file in files:
+        file_string = os.path.join(from_location, file)
+        dest_file_string = os.path.join(to_location, file)
+        if os.path.isfile(file_string):
+            os.makedirs(os.path.dirname(dest_file_string), exist_ok=True)
+            shutil.copyfile(file_string, dest_file_string)
+        else:
+            os.makedirs(dest_file_string, exist_ok=True)
+            copyfiles(file_string, dest_file_string)
+        print(f"copied file {file_string}")
+
 
 def main():
-    source_to_static()
+    source_to_destination()
 
 
 main()
